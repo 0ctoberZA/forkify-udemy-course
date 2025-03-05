3aa7ef19b42ec60050905057423a2264c282db15
0ctoberZA <tsakanilindsay@gmail.com>
2025-03-05 10:54:23 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9149af1f-f74e-4e29-8c9c-3e2bd11416b3';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,22 @@
+import View from './View.js';
+import previewView from './previewView.js';
+import icons from 'url:../../img/icons.svg';
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query!. Please try another one!';
+  _message = '';
+
+  _generateMarkup() {
+    console.log(this._data);
+    return this._data.map(this._generateMarkupPreview).join('');
+  }
+  _generateMarkup() {
+    console.log(this._data);
+    return this._data
+      .map(result => previewView.render(bookmark, false))
+      .join('');
+  }
+}
+
+export default new ResultsView();
